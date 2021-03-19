eb76c8f55fa67321c732dad2de2296413d4512a6
антон надькин <anton.nadikin@gmail.com>
2021-03-19 18:39:41 +0300

    альфа

---
+++ graphClient/UserResolver.cs
@@ -0,0 +1,77 @@
+﻿using GraphQL;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ranksClient
+{
+    public class UserResolver : ResolverBase
+    {
+        protected override GraphQLRequest QueryString
+        {
+            get => new GraphQLRequest
+            {
+                Query = @"{Users{name, secname,picture,groupId,rankId,about}}"
+            };
+        }
+        protected override GraphQLRequest MutationStringCreate
+        {
+            get => new GraphQLRequest
+            {
+                Query = @"mutation($user: UserInput){AddUser(User: $user){
+                        id,name,secname,groupId,rankId,isAdmin,password,picture,about}}"
+            };
+        }
+        protected override GraphQLRequest MutationStringUpdate
+        {
+            get => new GraphQLRequest
+            {
+                Query = @"mutation($user: UserInput){UpdateUser(User: $user){
+                        id,name,secname,groupId,rankId,isAdmin,password,picture,about}}"
+            };
+        }
+
+        public async Task<List<UserInputType>> GetUsers()
+        {
+            var response = await GetList(() => new { Users = new List<UserInputType> { } });
+            return (response.Users);
+        }
+        public async Task<UserInputTypeWithId> AddUser(UserInputType user)
+        {
+            var response = await Create(new{user=user}, () => new { AddUser = new UserInputTypeWithId() });
+            return (response.AddUser);
+        }
+        public async Task<UserInputType> UpdateUser(UserInputTypeWithId user)
+        {
+            var response = await Update(new { user = user }, () => new { UpdateUser = new UserInputType() });
+            return (response.UpdateUser);
+        }
+    }
+    public class BigUserType: User
+    { 
+        public GroupType groupInfo { get; set; }
+        public RankType rankInfo { get; set; }
+    }
+    public class UserInputType : User
+    {
+        public string groupId { get; set; }
+        public string rankId { get; set; }
+    }
+    public class UserInputTypeWithId: User
+    {
+
+        public string id { get; set; }
+        public string groupId { get; set; }
+        public string rankId { get; set; }
+    }
+    public class User
+    {
+        public string name { get; set; }
+        public string secname { get; set; }
+        public string isAdmin { get; set; }
+        public string password { get; set; }
+        public string picture { get; set; }
+        public string about { get; set; }
+    }
+
+}
---
+++ testing/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using ranksClient;
+using System.Threading.Tasks;
+
+namespace testing
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            GroupResolver group = new();
+            GroupType gr = new()
+            {
+                about="asdasd",
+                name="asd"
+            };
+            UserResolver user = new();
+           //var a = await group.AddGroup(gr);
+            UserInputTypeWithId newUser = new()
+            {
+                id="1",
+                name = "Антонидас",
+                secname = "Зубенко",
+                groupId="1",
+                rankId="2"
+            };
+            UserInputType newUs = new()
+            {
+                name = "Антонидасfromsharp",
+                secname = "Зубенко",
+                groupId = "1",
+                rankId = "2",
+                isAdmin = "2",
+                password = "asasd",
+                about= "dasdasas"
+            };
+            UserInputTypeWithId s = await user.AddUser(newUs);
+            Console.WriteLine(s.id);
+            var userList = await user.GetUsers();
+            foreach (UserInputType u in userList)
+            {
+                Console.WriteLine(u.name);
+                //Console.WriteLine(u.);
+                Console.WriteLine(u.about);
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
